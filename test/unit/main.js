e0c688ce743e5171c1ea1a75fe22bc059da88c26
Amit Kumar <themit@gmail.com>
2014-11-18 17:18:29 -0500

    lesson 3 unit tests

--- test/unit/main.js
+++ test/unit/main.js
@@ -8,8 +8,34 @@ describe('controllers', function(){
 
   it('should define more than 5 awesome things', inject(function($controller) {
     var mainCtrl = $controller('MainCtrl', {});
-    expect(angular.isArray(mainCtrl.awesomeThings)).toBeFalsy();
+
+    expect(angular.isArray(mainCtrl.awesomeThings)).toBeTruthy();
     expect(mainCtrl.awesomeThings.length > 5).toBeTruthy();
+  }));
+
+  it('should have exactly 9 things', inject(function($controller) {
+    var ctrl = $controller('MainCtrl', {});
+
+    expect(ctrl.awesomeThings.length === 9).toBeTruthy();
+  }));
+
+  it('should display items in expected positions', inject(function($controller) {
+    var ctrl = $controller('MainCtrl', {});
+
+    expect(ctrl.awesomeThings[0].key).toMatch('angular');
+  }));
+
+  it('should have all expected properties on each item', inject(function($controller) {
+    var ctrl = $controller('MainCtrl', {});
+
+    ctrl.awesomeThings.forEach(function(item){
+      expect(item.key).toBeDefined();
+      expect(item.title).toBeDefined();
+      expect(item.url).toBeDefined();
+      expect(item.description).toBeDefined();
+      expect(item.logo).toBeDefined();
+    });
+
   }));
 
 });
