fa8ea352be80c4adb5005665b14dcf149914c922
Amit Kumar <themit@gmail.com>
2014-11-19 17:31:30 -0500

    first unit test for todos

--- test/unit/main.js
+++ test/unit/main.js
@@ -1,41 +1,18 @@
 'use strict';
 
 describe('controllers', function(){
+  var scope;
   beforeEach(module('angularjsTutorial'));
 
   beforeEach(inject(function($rootScope) {
+    scope = $rootScope.$new();
   }));
 
-  it('should define more than 5 awesome things', inject(function($controller) {
-    var mainCtrl = $controller('MainCtrl', {});
-
-    expect(angular.isArray(mainCtrl.awesomeThings)).toBeTruthy();
-    expect(mainCtrl.awesomeThings.length > 5).toBeTruthy();
-  }));
-
-  it('should have exactly 9 things', inject(function($controller) {
-    var ctrl = $controller('MainCtrl', {});
-
-    expect(ctrl.awesomeThings.length === 9).toBeTruthy();
-  }));
-
-  it('should display items in expected positions', inject(function($controller) {
-    var ctrl = $controller('MainCtrl', {});
-
-    expect(ctrl.awesomeThings[0].key).toMatch('angular');
-  }));
-
-  it('should have all expected properties on each item', inject(function($controller) {
-    var ctrl = $controller('MainCtrl', {});
-
-    ctrl.awesomeThings.forEach(function(item){
-      expect(item.key).toBeDefined();
-      expect(item.title).toBeDefined();
-      expect(item.url).toBeDefined();
-      expect(item.description).toBeDefined();
-      expect(item.logo).toBeDefined();
+  it('should define a todos array', inject(function($controller) {
+    var mainCtrl = $controller('MainCtrl', {
+      $scope : scope
     });
 
+    expect(angular.isArray(mainCtrl.todos)).toBeTruthy();
   }));
-
 });
