9b476faf961d8d18067d728436243e7b9d4a30a8
Amit Kumar <themit@gmail.com>
2014-12-03 17:35:53 -0500

    unit tests for TodoService-as-promises

--- test/unit/components/todo/todo.service.js
+++ test/unit/components/todo/todo.service.js
@@ -9,56 +9,68 @@ describe('TodoService', function(){
     TodoService = _TodoService_;
   }));
 
+
   describe('#getTodos', function(){
     it('should return an array', function(done) {
-      var todos = TodoService.getTodos();
-      expect(angular.isArray(todos)).toBeTruthy();
+      TodoService.getTodos()
+      .then(function(todos){
+        expect(angular.isArray(todos)).toBeTruthy();
+        done();
+      });
     });
   });
 
 
   describe('#addTodo', function(){
     it('should be able to add a todo and return the newly created todo', function(done) {
-      var newTodo = TodoService.addTodo({
+      TodoService.addTodo({
         title : 'test title 1'
+      })
+      .then(function(newTodo){
+        expect(TodoService.getTodos().length === 1).toBeTruthy();
+        expect(newTodo).toBeDefined();
+        done();
       });
-
-      expect(TodoService.getTodos().length === 1).toBeTruthy();
-      expect(newTodo).toBeDefined();
     });
 
     it('should create "title" and "completed" properties on todos', inject(function($controller) {
       TodoService.addTodo({
         title : 'test title 2'
+      })
+      .then(TodoService.getTodos)
+      .then(function(todos){
+        expect(todos.length === 1).toBeTruthy();
+
+        expect(todos[0].title).toBeDefined();
+        expect(todos[0].completed).toBeDefined();
+        expect(todos[0].completed).toBe(false);
+        done();
       });
-
-      var todos = TodoService.getTodos();
-
-      expect(todos.length === 1).toBeTruthy();
-
-      expect(todos[0].title).toBeDefined();
-      expect(todos[0].completed).toBeDefined();
-      expect(todos[0].completed).toBe(false);
     }));
-
-
   });
+
 
   describe('#removeTodoById', function(){
     it('should be able to remove a todo by reference', inject(function($controller) {
 
       var title = 'test title 3';
 
-      var newTodo = TodoService.addTodo({
+      TodoService.addTodo({
         title : title
+      })
+      .then(TodoService.getTodos)
+      .then(function(todos){
+        expect(todos.length === 1).toBeTruthy();
+        return;
+      })
+      .then(function(){
+        return TodoService.removeTodoById(newTodo.id)
+      })
+      .then(TodoService.getTodos)
+      .then(function(todos){
+        expect(todos.length === 0).toBeTruthy();
+        done();
       });
-
-      expect(TodoService.getTodos().length === 1).toBeTruthy();
-
-      TodoService.removeTodoById(newTodo.id);
-
-      // expect(true).toBeTruthy();
-      expect(TodoService.getTodos().length === 0).toBeTruthy();
     }));
   });
 
