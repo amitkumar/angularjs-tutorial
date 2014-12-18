0bee38644d2d918cb3fbb9aa6dca8224deaa9c7d
Amit Kumar <themit@gmail.com>
2014-12-17 19:57:49 -0500

    todo-list directive using AngularFire service

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,55 +1,23 @@
 'use strict';
 
 angular.module('angularjsTutorial')
-  .controller('MainCtrl', ['$scope', '$log', '$q', 'Todo', function ($scope, $log, $q, Todo) {
+  .controller('MainCtrl', ['$scope', '$log', '$q', 'TodoFireService', function ($scope, $log, $q, TodoFireService) {
     $log.log('MainCtrl instantiated');
     var self = this;
 
     self.newTodoTitle = '';
 
-
-    self.getTodos = function(){
-       return self.todos = Todo.query();
-    };
-
     self.addTodo = function(options){
-      var newTodoOptions = {
-        title : options.title,
-        completed : false
-      };
-      Todo.add(newTodoOptions, function(newTodo){
-        $log.log('MainCtrl $add newTodo response', newTodo);
+      var newTodo;
 
-        // Move the key to the $id property, like our view expects
-        newTodo.$id = newTodo.name;
-        newTodo.name = undefined;
-
-        // Firebase only returns the key of the newly created object, not the entire object
-        // So we need to add our properties back on to it
-        angular.extend(newTodo, newTodoOptions);
-        $log.log('MainCtrl $add newTodo after hydration', newTodo);
-
-        self.todos.push(newTodo);
+      return TodoFireService.addTodo(options)
+      .then(function(newTodoResult){
+        newTodo = newTodoResult;
         self.newTodoTitle = '';
+      },
+      function(err){
+        $log.log(err);
       });
     };
 
-
-    self.removeTodo = function(todo){
-      return todo.$remove(function(){
-        self.todos.splice(self.todos.indexOf(todo), 1);
-      });
-    };
-
-    self.getTodoClasses = function(todo){
-      return {
-        'completed' : todo.completed
-      }
-    };
-
-    self.saveTodo = function(todo){
-      return todo.$update();
-    }
-
-    self.getTodos();
   }]);
