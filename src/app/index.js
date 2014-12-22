f2c204cae6560006a52e75d7588823faf147de4a
Amit Kumar <themit@gmail.com>
2014-12-22 16:54:12 -0500

    introducing route security through auth enforcement - redirecting user back to login if not authed on protected route - storing user on GlobalCtrl for centralized access - updating navbar with login/logout links

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,27 @@
 'use strict';
 
 angular.module('angularjsTutorial', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'firebase'])
+  .constant('firebaseUrl', 'https://ak-angularjstutorial.firebaseio.com/')
+
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
         url: '/',
         templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl as mainCtrl'
+        controller: 'MainCtrl as mainCtrl',
+        resolve : {
+          'user' : ['AuthService', function(AuthService){
+            return AuthService.requireUser();
+          }]
+        }
       })
+
+      .state('login', {
+        url: '/login?redirect',
+        templateUrl: 'app/login/login.html',
+        controller: 'LoginCtrl as loginCtrl'
+      })
+
       .state('contact', {
         url: '/contact',
         templateUrl: 'app/contact/contact.html',
@@ -17,9 +31,40 @@ angular.module('angularjsTutorial', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSan
     $urlRouterProvider.otherwise('/');
   })
 
-  .controller('GlobalCtrl', function(){
-    this.globalObject = {
+  .run(['$rootScope', '$log', '$state',
+    function($rootScope, $log, $state) {
+      // routes may reject resolve promises with our custom {authRequired: true} error
+      $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+        $log.log('$stateChangeError', event, toState, toParams, fromState, fromParams, error);
+
+        if( error === 'AUTH_REQUIRED' ) {
+          $log.log('$stateChangeError, redirecting to login', toState.name);
+          $state.go('login', { redirect : toState.name });
+        }
+      });
+    }
+  ])
+
+
+  .controller('GlobalCtrl', ['AuthService', '$log', '$state', function(AuthService, $log, $state){
+    var self = this;
+
+    AuthService.onAuth(function(user){
+      $log.log('AuthService.onAuth', user);
+      self.user = user;
+    });
+
+    $log.log('User is ', this.user);
+
+    self.logout = function(){
+      AuthService.logout().then(function(){
+        $log.log('GlobalCtrl AuthService.logout');
+        $state.go('home');
+      });
+    };
+
+    self.globalObject = {
       message : 'Global'
     };
-  })
+  }])
 ;
