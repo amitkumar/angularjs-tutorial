0b10b20128a2f8b1ab35d5ed3b70f660520be08a
Amit Kumar <themit@gmail.com>
2015-01-09 15:30:22 -0500

    first end-to-end tests: home page redirect & login

--- test/e2e/main.js
+++ test/e2e/main.js
@@ -3,13 +3,48 @@
 describe('The main view', function () {
 
   beforeEach(function () {
-    browser.get('http://localhost:3000/index.html');
+    browser.get('https://localhost:3000');
   });
 
-  it('list more than 5 awesome things', function () {
-    element.all(by.repeater('awesomeThing in mainCtrl.awesomeThings')).count().then(function(count) {
-      expect(count > 5).toBeTruthy();
+
+  describe('when not logged in', function(){
+    it('redirects user to login page', function () {
+      expect(browser.getCurrentUrl()).toEqual('https://localhost:3000/#/login?redirect=home');
     });
   });
 
+
+  describe('when logged in', function(){
+
+    beforeEach(function () {
+      element(by.model('loginCtrl.email')).sendKeys('test@test.com');
+      element(by.model('loginCtrl.password')).sendKeys('test');
+
+      // Submit the form. Could use one of the following:
+      // element(by.model('loginCtrl.password')).sendKeys(protractor.Key.ENTER);
+      // element(by.model('loginCtrl.password')).submit(); // submit whatever form this element exists on
+      // element(by.buttonText('Login')).click();
+      element(by.buttonText('Login')).click();
+
+      browser.wait(function() {
+          // keeps waiting until the returned promise resolves to true
+          return browser.getCurrentUrl().then(function(url){
+            if (url === 'https://localhost:3000/#/'){
+              return true;
+            } else {
+              return;
+            }
+          });
+        },
+        2000,
+        'browser never redirected to home'
+      );
+    });
+
+    it('shows the home page', function(){
+      expect(browser.getCurrentUrl()).toEqual('https://localhost:3000/#/');
+    });
+
+
+  });
 });
